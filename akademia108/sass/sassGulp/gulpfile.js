3e77ec75cdb2187ac463f20678f017213fe717ef
Marcin Hibner <marcin.hibner@o2.pl>
2018-03-20 21:10:33 +0100

    dodanie-sass

---
+++ akademia108/sass/sassGulp/gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp'); 
+var sass = require('gulp-sass');
+var cssbeautify = require('gulp-cssbeautify');
+ 
+gulp.task('sass', function(){   
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())  
+        .pipe(cssbeautify())
+        .pipe(gulp.dest('app/css')); 
+}); 
+ 
+gulp.task('watch', function(){   
+    gulp.watch('app/scss/**/*.scss', ['sass']);  
+}); 
+
+gulp.task('hello', function () {
+    console.log('Czesc Marcin');
+});
